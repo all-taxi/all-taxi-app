161cd72790e52ec57ca768123a5a043c2da2ac36
kimgoat <kimgoatlove@gmail.com>
2024-08-28 14:42:40 +0900

    chore: 네비게이션 관련 타입 초기 세팅

---
+++ src/types/navigation.ts
@@ -0,0 +1,10 @@
+import { RouteName, ROUTES } from "../constants/routes";
+import { StackNavigationProp } from "@react-navigation/stack";
+
+export type RootStackParamList = {
+  [ROUTES.HOME]: undefined;
+  [ROUTES.DETAIL]: undefined;
+  // 다른 라우트들...
+};
+
+export type NavigationProp = StackNavigationProp<RootStackParamList, RouteName>;
