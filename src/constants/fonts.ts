e884a07ed1c1708cfa1eef51f86bc3f7446c8918
kimgoat <kimgoatlove@gmail.com>
2024-08-28 17:21:32 +0900

    chore: 폰트 파일 상수로 관리

---
+++ src/constants/fonts.ts
@@ -0,0 +1,15 @@
+export const FONT_FILES = {
+  "Pretendard-Regular": require("@assets/fonts/Pretendard/Pretendard-Regular.ttf"),
+  "Pretendard-Thin": require("@assets/fonts/Pretendard/Pretendard-Thin.ttf"),
+  "Pretendard-Bold": require("@assets/fonts/Pretendard/Pretendard-Bold.ttf"),
+  "NotoSansKR-Black": require("@assets/fonts/NotoSansKR/NotoSansKR-Black.ttf"),
+  "NotoSansKR-Bold": require("@assets/fonts/NotoSansKR/NotoSansKR-Bold.ttf"),
+  "NotoSansKR-ExtraBold": require("@assets/fonts/NotoSansKR/NotoSansKR-ExtraBold.ttf"),
+  "NotoSansKR-ExtraLight": require("@assets/fonts/NotoSansKR/NotoSansKR-ExtraLight.ttf"),
+  "NotoSansKR-Light": require("@assets/fonts/NotoSansKR/NotoSansKR-Light.ttf"),
+  "NotoSansKR-Medium": require("@assets/fonts/NotoSansKR/NotoSansKR-Medium.ttf"),
+  "NotoSansKR-Regular": require("@assets/fonts/NotoSansKR/NotoSansKR-Regular.ttf"),
+  "NotoSansKR-SemiBold": require("@assets/fonts/NotoSansKR/NotoSansKR-SemiBold.ttf"),
+  "NotoSansKR-Thin": require("@assets/fonts/NotoSansKR/NotoSansKR-Thin.ttf"),
+  // 새로 추가한 폰트들을 여기에 추가
+};
