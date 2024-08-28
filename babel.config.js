dd5144cba29f4aef98c15577646d0434268e9f8f
kimgoat <kimgoatlove@gmail.com>
2024-08-28 15:14:10 +0900

    chore: 절대 경로 설정

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@constants": "./src/constants",
+            "@hooks": "./src/hooks",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@styles": "./src/styles",
+            "@types": "./src/types",
+            "@utils": "./src/utils",
+            // 필요한 다른 경로들을 여기에 추가
+          },
+        },
+      ],
+    ],
   };
 };
