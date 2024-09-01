2dd669adb41ccbe2559ccbbb0cd6c7e9b25d3c00
김영서 <kimgoatlove@gmail.com>
2024-09-01 22:51:31 +0900

    chore: 프로젝트 세팅 (#30)
    * chore: .env에 환경변수 설정
    
    * chore: 필요한 라이브러리 설치 및 설정
    
    * chore: 절대경로 추가

--- babel.config.js
+++ babel.config.js
@@ -18,8 +18,20 @@ module.exports = function (api) {
             "@styles": "./src/styles",
             "@types": "./src/types",
             "@utils": "./src/utils",
+            "@states": "./src/states",
             // 필요한 다른 경로들을 여기에 추가
           },
+        },
+      ],
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
         },
       ],
     ],
---
+++ env.d.ts
@@ -0,0 +1,7 @@
+declare module "@env" {
+  export const KAKAO_REST_API_KEY: string;
+  export const VOICE_CHAT_URL: string;
+  export const GOOGLE_SPEECH_TO_TEXT_API_KEY: string;
+  export const NAVER_API_KEY_ID: string;
+  export const NAVER_API_KEY: string;
+}
