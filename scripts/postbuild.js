ab1049d827ac86b98c12a82d1ae78edae3958682
김영서 <kimgoatlove@gmail.com>
2024-09-01 16:19:37 +0900

    fix: assets 경로 수정 (#29)
    assets을 불러오는 경로 수정을 위한 스크립트 작성

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -12,10 +12,42 @@ if (!indexContent.includes('<base href="/all-taxi-app/">')) {
   );
 }
 
-// 스크립트 경로 수정
-indexContent = indexContent.replace(
-  'src="/_expo/',
-  'src="/all-taxi-app/_expo/'
-);
+// 스크립트 및 에셋 경로 수정
+indexContent = indexContent.replace(/src="\//g, 'src="/all-taxi-app/');
+indexContent = indexContent.replace(/href="\//g, 'href="/all-taxi-app/');
 
 fs.writeFileSync(indexPath, indexContent);
+
+// 폰트 파일 경로 수정
+const assetsPath = path.join(__dirname, "..", "dist");
+function processDirectory(directory) {
+  fs.readdirSync(directory, { withFileTypes: true }).forEach((dirent) => {
+    const fullPath = path.join(directory, dirent.name);
+    if (dirent.isDirectory()) {
+      processDirectory(fullPath);
+    } else if (
+      dirent.isFile() &&
+      (dirent.name.endsWith(".js") || dirent.name.endsWith(".css"))
+    ) {
+      try {
+        let content = fs.readFileSync(fullPath, "utf8");
+        content = content.replace(/\/assets\//g, "/all-taxi-app/assets/");
+        content = content.replace(
+          /\/src\/all-taxi-app\/assets\//g,
+          "/src/assets/"
+        );
+
+        // 중복 경로가 생성되지 않도록 확인
+        content = content.replace(
+          /\/all-taxi-app\/all-taxi-app\//g,
+          "/all-taxi-app/"
+        );
+        fs.writeFileSync(fullPath, content);
+      } catch (err) {
+        console.error(`Error processing file ${fullPath}:`, err);
+      }
+    }
+  });
+}
+
+processDirectory(assetsPath);
