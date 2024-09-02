11bc1f04267d26f2e56e386656eedcbbc31bfe9e
kimgoat <kimgoatlove@gmail.com>
2024-09-03 03:03:24 +0900

    chore: Home Screen 수정

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -13,11 +13,11 @@ const HomeScreen = ({ navigation }: Props) => {
   return (
     <View style={styles.container}>
       <View style={styles.map}>
-        <MapView />
+        {/* <MapView /> */}
         {/* <MapView x="126.978" y="37.5665" /> */}
       </View>
 
-      <AudioGuideHeader message="어디로 갈까요?" />
+      <AudioGuideHeader message="어디로 갈까요?" shouldSpeak={true} />
       <NavButton onPress={() => navigation.navigate(ROUTES.VOICE_CHAT)} />
     </View>
   );
